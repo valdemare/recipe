ccd7bbabf865b5bde7c9177986ed90a377b880be
valdemare <valdemare@mail.ru>
2018-06-14 14:00:56 +0300

    number format exception handler added

--- src/test/java/com/valdemare/recipe/controllers/RecipeControllerTest.java
+++ src/test/java/com/valdemare/recipe/controllers/RecipeControllerTest.java
@@ -69,6 +69,19 @@ public class RecipeControllerTest {
     }
 
     @Test
+    public void testGetRecipeNuberFormat() throws Exception {
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        when(recipeService.findById(anyLong())).thenThrow(NumberFormatException.class);
+
+        mockMvc.perform(get("/recipe/asdf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
+
+    @Test
     public void testGetNewRecipeForm() throws Exception {
         RecipeCommand command = new RecipeCommand();
 
